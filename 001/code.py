92ebe9c38e1fdd8401799d7e163350390b5684fc
Benjamin Park <89424717+b3npaii@users.noreply.github.com>
2021-08-28 18:44:08 +0000

    Added new functions and test cases

--- 001/code.py
+++ 001/code.py
@@ -1,3 +1,5 @@
+import math
+
 def sum_of_n_numbers(n):
     sum = 0
     for i in range(1, n+1):
@@ -22,10 +24,88 @@ def convert_to_numbers(input_string):
 
 
 def is_prime(n):
-    for m in range(2, n//2):
+    for m in range(2, n//2 + 1):
         if n % m == 0:
             return "False"
-        else:
+        elif m == n//2:
             return "True"
+print(is_prime(13))
 
 
+def get_intersection(list1, list2):
+    answer = []
+    for element in list1:
+        if element in list2:
+            if element not in answer:
+                answer.append(element)
+    return answer
+
+print(get_intersection([1,2,3,1],[1,2,3]))
+
+
+def get_union(list1, list2):
+    answer_array = []
+    for element in list1:
+        answer_array.append(element)
+    for element in list2:
+        if element not in list1:
+            answer_array.append(element)
+    return answer_array
+
+
+def count_characters(input_string):
+    answer_dict = {}
+    lowercase_string = input_string.lower()
+    for letter in lowercase_string:
+        answer_dict[str(letter)] = lowercase_string.count(letter)
+    return answer_dict
+
+
+def get_first_n_terms_nonrecursive(n):
+    answer_array = []
+    for i in range(1, n+1):
+        if i == 1:
+            answer_array.append(5)
+            previous_term = 5
+        else:
+            previous_term = 3*previous_term - 4
+            answer_array.append(previous_term)
+    return answer_array
+
+
+def get_n_term_recursive(n):
+    if n == 1:
+        return 5
+    return(get_n_term_recursive(n-1)*3 - 4)
+
+print(get_n_term_recursive(4))
+
+
+def convert_to_base_10(n):
+    n_digit_count = len(str(n))
+    string_n = str(n)
+    reverse_n = string_n[::-1]
+    output = 0
+    for i in range(0, n_digit_count):
+        output = output + int(reverse_n[i]) * 2**i
+    return output
+
+
+def nth_fibonacci_number(n): 
+    if n == 0:
+        return 0
+    if n== 1:
+        return 1
+    return(nth_fibonacci_number(n-1)+nth_fibonacci_number(n-2))
+
+
+def convert_to_base_2(n):
+    exponent_list = []
+    output = 0
+    while n > 0:
+        n_log_2_rounded = math.floor(math.log(n,2))
+        exponent_list.append(n_log_2_rounded)
+        n = n - 2**n_log_2_rounded
+    for exponent in exponent_list: 
+        output += 10**exponent
+    return output
--- 001/tests.py
+++ 001/tests.py
@@ -2,18 +2,72 @@ from code import word_symmetry
 from code import sum_of_n_numbers
 from code import convert_to_numbers
 from code import is_prime
+from code import get_intersection
+from code import get_union
+from code import count_characters
+from code import get_first_n_terms_nonrecursive
+from code import get_n_term_recursive
+from code import convert_to_base_10
+from code import nth_fibonacci_number
+from code import convert_to_base_2
+
 
 if word_symmetry("thing") == "True":
     print("word_symmetry is not working")
-if word_symmetry("taddat") == "True":
+
+if word_symmetry("!taddat!") == "True":
     print("word_symmetry is working")
+
 if is_prime(20) == "False":
     print("is_prime is working")
+
+if is_prime(23) == "True":
+    print("is_prime is working")
+
 if convert_to_numbers("abc") != [1, 2, 3]:
     print("convert_to_numbers is not working")
-if convert_to_numbers("abc") == [1, 2, 3]:
+
+if convert_to_numbers(" qwe") == [0, 17, 5]:
     print("convert_to_numbers is working")
-if sum_of_n_numbers(10) != 55:
-    print("sum_of_n_numbers is not working")
+
+if sum_of_n_numbers(500) == 125250:
+    print("sum_of_n_numbers is working")
+
 if sum_of_n_numbers(10) == 55:
     print("sum_of_n_numbers is working")
+
+if get_intersection([1, 2, 3, 4, 5], [4, 5, 6]) == [4, 5]:
+    print("get_intersection is working")
+
+if get_intersection(["abc", 4, "17"], ["abc", 8, "jk"]) == ["abc"]:
+    print("get_intersection is working")
+
+if get_union([1, 2, 3, 4, 5], [45, 6, 7, 5]) == [1, 2, 3, 4, 5, 45, 6, 7]:
+    print("get_union is working")
+
+if get_union([1, 2, 3, "abc1"], [7, "abc1"]) == [1, 2, 3, "abc1", 7]:
+    print("get_union is working")
+
+array = get_first_n_terms_nonrecursive(6)
+
+if array[5] != get_n_term_recursive(6):
+    print("broke")
+else:
+    print("work")
+
+if get_first_n_terms_nonrecursive(3) == [5, 11, 29]:
+    print("get_first_n_terms_nonrecursive is working")
+
+if get_n_term_recursive(3) == 29:
+    print("get_n_term_recursive is wokring")
+
+if convert_to_base_10(convert_to_base_2(10)) != 10:
+    print("one of the conversions is broke")
+else:
+    print("conversions are working")
+
+if convert_to_base_2(convert_to_base_10(10110)) != 10110:
+    print("conversions are not wokring")
+else:
+    print("conversions are working")
+
